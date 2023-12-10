ec1806b73dd3662b20c23607a44508b28a1ae36a
Sameer Shekhar <105191950+SameerShekhar2003@users.noreply.github.com>
2023-12-10 09:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/KIIT/Desktop/Disease Prediticion/Saved Models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/KIIT/Desktop/Disease Prediticion/Saved Models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/KIIT/Desktop/Disease Prediticion/Saved Models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
